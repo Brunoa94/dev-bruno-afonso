b80f9ecc8e31eb476b75f42bd3369bf91072b103
Bruno Afonso <bruno.afonso94@hotmail.com>
2024-05-26 19:23:43 +0100

    Padding x added

--- src/index.js
+++ src/index.js
@@ -7,9 +7,6 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <head>
-      <title>Bruno Afonso - Frontend Portfolio</title>
-    </head>
     <App />
   </React.StrictMode>
 );
