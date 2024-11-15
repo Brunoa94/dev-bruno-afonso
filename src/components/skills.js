0f02b44edf4038036d2d13632c0fbdd16ee316c7
Bruno Afonso <bruno.afonso94@hotmail.com>
2024-11-15 02:14:16 +0000

    Skills updated

--- src/components/skills.js
+++ src/components/skills.js
@@ -7,7 +7,7 @@ import { BiLogoTypescript } from "react-icons/bi";
 
 const Skills = () => {
 
-    const Skill = (name, x, y, color, icon) => {
+    const Skill = (name, x, y, color, icon, text) => {
         const ref = useRef(null);
         const isInView = useInView(ref, { once: true });
 
@@ -20,9 +20,10 @@ const Skills = () => {
                 x: x, y: y
             }: {}}
             transition={{duration: 2.5}}
-            className={`flex items-center justify-center rounded-full font-semibold text-white p-5 shadow-dark cursor-pointer absolute bg-spheres h-20 w-20 shadow-2xl`}>
+            className={`flex items-center justify-center rounded-full font-semibold text-white p-5 shadow-dark cursor-pointer absolute bg-spheres h-24 w-24 shadow-2xl flex-col`}>
                 
                 {icon}
+                <span className='text-xs mt-1'>{text}</span>
             </motion.div>
         )
     }
@@ -34,15 +35,15 @@ const Skills = () => {
             <motion.div whileHover={{scale: 1.1}} className='flex items-center justify-center h-20 w-20 bg-yellow-500 rounded-full font-semibold text-white p-5 shadow-dark cursor-pointer'>
                 Web
             </motion.div>
-            {Skill("CSS", -50, 100, "bg-yellow-700", <FaCss3Alt className='text-4xl'/>)}
-            {Skill("Tailwind CSS", 50, -100, "bg-yellow-700", <SiTailwindcss className='text-4xl'/>)}
-            {Skill("HTML", -150, -150, "bg-yellow-700", <SiHtml5 className='text-4xl'/>)}
-            {Skill("NextJS", 240, -140, "bg-yellow-700", <TbBrandNextjs className='text-4xl'/>)}
-            {Skill("React", -250, 120, "bg-yellow-700", <FaReact className='text-4xl'/>)}
-            {Skill("Typescript", -350, 0, "bg-orange-700", <BiLogoTypescript className='text-4xl'/>)}
-            {Skill("Github", 350, 0, "bg-orange-700", <FaGithub className='text-4xl'/>)}
-            {Skill("Ruby on Rails", 260, 180, "bg-red-800", <SiRubyonrails className='text-4xl'/>)}
-            {Skill("NodeJS", -300, -170, "bg-red-800", <FaNodeJs className='text-4xl'/> )}
+            {Skill("CSS", -50, 100, "bg-yellow-700", <FaCss3Alt className='text-3xl shrink-0'/>, "CSS")}
+            {Skill("Tailwind CSS", 50, -100, "bg-yellow-700", <SiTailwindcss className='text-3xl shrink-0'/>, "Tailwind")}
+            {Skill("HTML", -150, -150, "bg-yellow-700", <SiHtml5 className='text-3xl shrink-0'/>, "HTML5")}
+            {Skill("NextJS", 240, -140, "bg-yellow-700", <TbBrandNextjs className='text-3xl shrink-0'/>, "NextJS")}
+            {Skill("React", -250, 120, "bg-yellow-700", <FaReact className='text-3xl shrink-0'/>, "ReactJS")}
+            {Skill("Typescript", -350, 0, "bg-orange-700", <BiLogoTypescript className='text-3xl shrink-0'/>, "Typescript")}
+            {Skill("Github", 350, 0, "bg-orange-700", <FaGithub className='text-3xl shrink-0'/>, "Github")}
+            {Skill("Ruby on Rails", 260, 180, "bg-red-800", <SiRubyonrails className='text-3xl shrink-0'/>, "Rails")}
+            {Skill("NodeJS", -300, -170, "bg-red-800", <FaNodeJs className='text-3xl shrink-0'/>, "NodeJS" )}
         </div>
     </div>
         <div className='flex items-center pt-6 w-full justify-center gap-3 lg:hidden'>
